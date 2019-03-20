5e15134f31be18bfd6c2dd8c8e799878818a7921
chetan882777 <chetan882777@gmail.com>
2019-03-20 07:25:54 -0700

    statelessWidget

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget{
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppStateKeeper();
+  }
+}
 
+class AppStateKeeper extends State<App> {
   var barColor = const Color(0xFFFF0000);
   var bgColor = const Color(0xFFDAE0E2);
+  int clicks = 0;
 
   @override
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: new Container(
+          margin: EdgeInsets.all(32.0),
+          decoration: new BoxDecoration(
+              color: Colors.red[200],
+              borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(16.0),
+                  topRight: const Radius.circular(16.0),
+                  bottomLeft: const Radius.circular(16.0),
+                  bottomRight: const Radius.circular(16.0))),
+          child: new Center(
+            child: Text(
+              '${clicks}',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
         backgroundColor: bgColor,
-        floatingActionButton: FloatingActionButton(onPressed: (){},
-            child: Icon(
-                Icons.add,
-                size: 32.0,
-                color: Colors.white),
-            backgroundColor: Colors.red[700]
-        ),
-        appBar: AppBar(
-            backgroundColor: barColor
-        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clicks++;
+              });
+            },
+            child: Icon(Icons.add, size: 32.0, color: Colors.white),
+            backgroundColor: Colors.red[700]),
+        appBar: AppBar(backgroundColor: barColor),
       ),
     );
   }
-
 }
